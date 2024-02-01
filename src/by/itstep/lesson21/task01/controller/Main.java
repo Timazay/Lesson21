a396cad74e59846c7122d6ef31d97e83778fc110
Timazay <oxxxymofey@gmail.com>
2024-02-01 21:16:29 +0300

    Add Calculator class with sum method

--- src/by/itstep/lesson21/task01/controller/Main.java
+++ src/by/itstep/lesson21/task01/controller/Main.java
@@ -1,7 +1,9 @@
 package by.itstep.lesson21.task01.controller;
 
+import by.itstep.lesson21.task01.model.logic.Calculator;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("testing...");
+        System.out.println(Calculator.sum(2, 4));
     }
 }
---
+++ src/by/itstep/lesson21/task01/model/logic/Calculator.java
@@ -0,0 +1,7 @@
+package by.itstep.lesson21.task01.model.logic;
+
+public class Calculator {
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
