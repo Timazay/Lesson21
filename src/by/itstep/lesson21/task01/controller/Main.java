c176a79d43760f1939afb65f52a9d693ec3fe0e9
Timazay <oxxxymofey@gmail.com>
2024-02-01 21:28:10 +0300

    Update Main class

--- src/by/itstep/lesson21/task01/controller/Main.java
+++ src/by/itstep/lesson21/task01/controller/Main.java
@@ -5,5 +5,7 @@ import by.itstep.lesson21.task01.model.logic.Calculator;
 public class Main {
     public static void main(String[] args) {
         System.out.println(Calculator.sum(2, 4));
+        System.out.println(Calculator.mul(2, 4));
+        System.out.println(Calculator.sub(2, 4));
     }
 }
